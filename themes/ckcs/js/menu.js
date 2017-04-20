fbd6eed4b26d7316413f1f6dd7cdd456f053d08e
Chris Morris <cmorris@ccistudios.com>
2017-04-20 14:19:24 -0400

    style the search block

--- themes/ckcs/js/menu.js
+++ themes/ckcs/js/menu.js
@@ -1,9 +1,15 @@
 (function($){
   $(function(){
     $(".menu--main .menu-item--expanded > a").on("click", dropdownClick);
+    $(".search-button").on("click", searchButtonClick);
   });
   function dropdownClick(e){
     e.preventDefault();
     $(this).closest("li").toggleClass("open");
+  }
+  function searchButtonClick(e){
+    e.preventDefault();
+    $(".search-button").hide();
+    $(".block-search").show().find("input[type=search]").focus();
   }
 })(jQuery);
