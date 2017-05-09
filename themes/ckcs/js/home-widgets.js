de5ab9898cb9e8fb2cebc38992e956f73841bfe8
Chris Morris <cmorris@ccistudios.com>
2017-05-09 13:04:43 -0400

    reponsive styling

--- themes/ckcs/js/home-widgets.js
+++ themes/ckcs/js/home-widgets.js
@@ -5,7 +5,22 @@
   function initCarousel(){
     $(".view-home-widgets .view-content").slick({
       arrows: true,
-      slidesToShow: 3
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 1,
+            adaptiveHeight: true
+          }
+        },
+      ]
     });
   }
 })(jQuery);
--- themes/ckcs/js/menu.js
+++ themes/ckcs/js/menu.js
@@ -2,6 +2,7 @@
   $(function(){
     $(".menu--main .menu-item--expanded > a").on("click", dropdownClick);
     $(".search-button").on("click", searchButtonClick);
+    $(".mobile-menu-button").on("click", mobileMenuButtonClick);
   });
   function dropdownClick(e){
     e.preventDefault();
@@ -11,5 +12,10 @@
     e.preventDefault();
     $(".search-button").hide();
     $(".block-search").show().find("input[type=search]").focus();
+  }
+  function mobileMenuButtonClick(e){
+    e.preventDefault();
+    $(".menu--main").toggle();
+    $("body").toggleClass("menu-open");
   }
 })(jQuery);
